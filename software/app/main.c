69cfa79e16f0937905138c395c3968c93f450201
tomaz-suller <tomaz.suller@usp.br>
2022-11-23 10:46:42 -0300

    Update main to write using generated functions

--- software/app/main.c
+++ software/app/main.c
@@ -16,6 +16,9 @@
 #define CLOCK_FREQUENCY 100e6
 #define UPDATE_PERIOD_IN_SECONDS 1/60
 
+#define X_OFFSET TABLE_WIDTH/2
+#define Y_OFFSET TABLE_HEIGHT/2
+
 int main(void)
 {
 #ifdef CONFIG_CPU_HAS_INTERRUPT
@@ -27,21 +30,40 @@ int main(void)
     time_init();
 
     int lastEvent = 0;
-    uint8_t control = 0;
-    int8_t rightPaddleDx = 0;
+    int16_t rightPaddleDx, leftPaddleDx = 0;
+    int16_t up, down, left, right = 0;
 
     while (1) {
 
         if (elapsed(&lastEvent, (CLOCK_FREQUENCY * UPDATE_PERIOD_IN_SECONDS))) {
+            up = button_up_output_read();
+            down = button_down_output_read();
+            left = button_left_output_read();
+            right = button_right_output_read();
 
+            if (up) rightPaddleDx = 1;
+            else if (down) rightPaddleDx = -1;
+            else rightPaddleDx = 0;
+
+            if (right) leftPaddleDx = 1;
+            else if (left) leftPaddleDx = -1;
+            else leftPaddleDx = 0;
 
             // control = getch();
             // if (control == 119) rightPaddleDx = 1; // w
             // else if (control == 115) rightPaddleDx = -1; // s
             // else rightPaddleDx = 0;
 
-            update(&pong, rightPaddleDx, 0);
+            update(&pong, rightPaddleDx, leftPaddleDx);
         }
     }
 	return 0;
+}
+
+void draw(game pong) {
+    drawer_cursor_right_py_write(pong.rightPaddle.y + Y_OFFSET);
+    drawer_cursor_left_py_write(pong.leftPaddle.y + Y_OFFSET);
+
+    drawer_ball_px_write(pong.ball.x + X_OFFSET);
+    drawer_ball_py_write(pong.ball.y + Y_OFFSET);
 }
