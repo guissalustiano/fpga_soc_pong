f6f4a6647f8e4e99539a61322c140962cf8a6e44
Eric Oliveira Gomes <ericog123456@gmail.com>
2022-11-28 20:01:59 -0300

    Update servo control to new clock frequency

--- hardware/custom/controle_servo.vhd
+++ hardware/custom/controle_servo.vhd
@@ -1,23 +1,3 @@
------------------Laboratorio Digital-------------------------------------
--- Arquivo   : circuito_pwm.vhd
--- Projeto   : Experiencia 1 - Controle de um servomotor
--------------------------------------------------------------------------
--- Descricao : 
---             codigo VHDL RTL gera saída digital com modulacao pwm
---
--- parametros de configuracao da saida pwm: CONTAGEM_MAXIMA e largura_pwm
--- (considerando clock de 50MHz ou periodo de 20ns)
---
--- CONTAGEM_MAXIMA=1250 gera um sinal periodo de 4 KHz (25us)
--- entrada LARGURA controla o tempo de pulso em 1:
--- 00=0 (saida nula), 01=pulso de 1us, 10=pulso de 10us, 11=pulso de 20us
--------------------------------------------------------------------------
--- Revisoes  :
---     Data        Versao  Autor             Descricao
---     26/09/2021  1.0     Edson Midorikawa  criacao
---     24/08/2022  1.1     Edson Midorikawa  revisao
--------------------------------------------------------------------------
---
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -33,8 +13,7 @@ end controle_servo;
 
 architecture rtl of controle_servo is
 
-  constant CONTAGEM_MAXIMA : integer := 1_000_000;  -- valor para frequencia da saida de 4KHz 
-                                               -- ou periodo de 25us
+  constant CONTAGEM_MAXIMA : integer := 2_000_000;
   signal contagem     : integer range 0 to CONTAGEM_MAXIMA-1;
   signal largura_pwm  : integer range 0 to CONTAGEM_MAXIMA-1;
   signal s_largura    : integer range 0 to CONTAGEM_MAXIMA-1;
@@ -73,9 +52,9 @@ begin
   begin
     case posicao is
 		-- bandeira abaixada
-      when '0' =>    s_largura <=    75_000;  -- pulso de  1.5 ms
+      when '0' =>    s_largura <=    150_000;  -- pulso de  1.5 ms
 		-- bandeira levantada
-      when '1' =>    s_largura <=   100_000;  -- pulso de 2 ms
+      when '1' =>    s_largura <=   200_000;  -- pulso de 2 ms
       when others =>  s_largura <=     0;  -- nulo   saida 0
     end case;
   end process;
